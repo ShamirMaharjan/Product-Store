808802bff4a8b98c35690fa66c9a82e52c30f847
Shamir Maharjan <shamirmaharjan07@gmail.com>
2024-12-12 20:12:53 +0545

    backend in progress

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,3 @@
-const port = 3000;
+export const port = 3000;
+export const mongodb = "mongodb://127.0.0.1:27017/";
 
-export default {
-    port
-}
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,66 @@
 import express from "express";
-import port from "./config.js";
+import { port, mongodb } from "./config.js";
+import mongoose from "mongoose";
+import { Product } from "./models/productModel.js";
+
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("hello world")
+
+//create prodcut
+app.post("/product", async (req, res) => {
+    const { title, description, price, image } = req.body;
+    try {
+        if (!title || !description || !price || !image) {
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+
+        }
+        const newproduct = {
+            title,
+            description,
+            price,
+            image
+        }
+        const product = await Product.create(newproduct);
+        return res.status(201).json(product);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
 })
 
-app.listen(port);
+//get all products
+app.get("/product", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        return res.status(200).json(products);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/product/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        return res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+mongoose.connect(mongodb).then(() => {
+    console.log("connected to database");
+    app.listen(port, () => {
+        console.log("server is running");
+    });
+
+}).catch((err) => {
+    console.log(err);
+})
