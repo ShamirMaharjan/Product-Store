cf2cd4d05c41a65b24256e16d30b441f6c93a0cc
Shamir Maharjan <shamirmaharjan07@gmail.com>
2024-12-12 21:52:47 +0545

    backend almost done

--- backend/index.js
+++ backend/index.js
@@ -1,59 +1,17 @@
 import express from "express";
 import { port, mongodb } from "./config.js";
 import mongoose from "mongoose";
-import { Product } from "./models/productModel.js";
+import productRoute from "./routes/productRoute.js";
 
 
 const app = express();
+
+//middleware for parsing request.body
 app.use(express.json());
 
+app.use("/product", productRoute);
 
-//create prodcut
-app.post("/product", async (req, res) => {
-    const { title, description, price, image } = req.body;
-    try {
-        if (!title || !description || !price || !image) {
-            return res.status(400).json({
-                message: "All fields are required"
-            })
 
-        }
-        const newproduct = {
-            title,
-            description,
-            price,
-            image
-        }
-        const product = await Product.create(newproduct);
-        return res.status(201).json(product);
-
-    } catch (error) {
-        console.log(error);
-
-    }
-
-})
-
-//get all products
-app.get("/product", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        return res.status(200).json(products);
-
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-app.get("/product/:id", async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        return res.status(200).json(product);
-
-    } catch (error) {
-        console.log(error);
-    }
-})
 
 mongoose.connect(mongodb).then(() => {
     console.log("connected to database");
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,94 @@
+import express from "express";
+import { Product } from "../models/productModel.js";
+const router = express.Router();
+
+
+//create prodcut
+router.post("/", async (req, res) => {
+    const { title, description, price, image } = req.body;
+    try {
+        if (!title || !description || !price || !image) {
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+
+        }
+        const newproduct = {
+            title,
+            description,
+            price,
+            image
+        }
+        const product = await Product.create(newproduct);
+        return res.status(201).json(product);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+})
+
+//get all products
+router.get("/", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        return res.status(200).json(products);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//read one product by id
+router.get("/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        return res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//update product by id
+router.put("/:id", async (req, res) => {
+    const { title, description, price, image } = req.body;
+    try {
+        if (!title || !description || !price || !image) {
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+
+        }
+
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+        if (!product) {
+            res.status(404).send("Product not found");
+        }
+        return res.status(200).json(product);
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+})
+
+//delete product by id
+router.delete("/:id", async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            res.status(404).send("Product not found");
+        }
+        return res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+
+    }
+
+})
+export default router;
